f0d63e8707b5373e3720cd7907a8e18a8cb32ca6
Ruslan <r.sevrukov@invokeinc.com>
2021-09-24 17:55:42 +0300

    fix error handling

--- app/controllers/cryptocurrency_controller.rb
+++ app/controllers/cryptocurrency_controller.rb
@@ -4,8 +4,8 @@ class CryptocurrencyController < ApplicationController
     @result = RequesterService.currencies(params[:cc_tickers])
     return render(json: @result, status: 200) if @result
     render json: "error", status: 400
-  #rescue
-  #  render json: "unhandled error", status: 500
+  rescue
+    render json: "unhandled error", status: 500
   end
 
   def specific_info
@@ -13,19 +13,19 @@ class CryptocurrencyController < ApplicationController
     return render(json: @result, status: 200) if @result
     render json: "error", status: 400
 
-    #rescue
-  #  render json: "unhandled error", status: 500
+  rescue
+    render json: "unhandled error", status: 500
   end
 
   def convert_cc_to_fiat
     render json: NomicsService.convert_cc_to_fiat(params[:cc_ticker], params[:fiat_ticker], params[:amount]), status: 200
-  #rescue
-  #  render json: "unhandled error", status: 500
+  rescue
+    render json: "unhandled error", status: 500
   end
 
   def convert_cc_to_cc
     render json: NomicsService.convert_cc_to_cc(params[:cc_from_ticker], params[:cc_to_ticker], params[:amount]), status: 200
-  #rescue
-  #  render json: "unhandled error", status: 500
+  rescue
+    render json: "unhandled error", status: 500
   end
 end
